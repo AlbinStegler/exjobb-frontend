ebb7eaba0859671e9c4b25644a501d87ed099e47
Albin Stegler <albin.stegler@gmail.com>
2024-04-13 16:25:31 +0200

    new ip to backend

--- egaming/src/models/url.js
+++ egaming/src/models/url.js
@@ -2,7 +2,7 @@ const url = {
     getUrl() {
         console.log(process.env.NODE_ENV)
         if (process.env.NODE_ENV === 'production') {
-            return 'http://16.171.24.207:1337'
+            return 'https://51.21.54.87.nip.io'
         } else {
             return 'http://localhost:1337'
         }
